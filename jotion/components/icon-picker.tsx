936cc3ef43b4022ab4cc29b9c106be984ee6a6f5
Brian Yin <byin@roblox.com>
2023-11-23 14:43:51 -0800

    Create icon-picker.tsx

---
+++ jotion/components/icon-picker.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import EmojiPicker, { Theme } from "emoji-picker-react";
+
+interface IconPickerProps {
+  onChange: (icon: string) => void;
+  children: React.ReactNode;
+}
+
+export const IconPicker: React.FC<IconPickerProps> = ({ onChange, children }) => {
+  return null;
+};
+
+export default IconPicker;
